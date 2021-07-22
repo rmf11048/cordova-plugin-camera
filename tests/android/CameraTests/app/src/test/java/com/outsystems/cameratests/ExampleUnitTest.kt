7d92cd5a3fc2b7e1c78ff737bc39dbed31a4aae9
Nelson Lopes Silva <5671236+nflsilva@users.noreply.github.com>
2021-07-22 12:43:06 +0100

    Changed android tests to Kotlin

---
+++ tests/android/CameraTests/app/src/test/java/com/outsystems/cameratests/ExampleUnitTest.kt
@@ -0,0 +1,21 @@
+package com.outsystems.cameratests
+
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, (2 + 2).toLong())
+    }
+
+    @Test
+    fun helloWorldTest() {
+        Assert.assertEquals("Hello World", "Hello World")
+    }
+}

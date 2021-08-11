b58a87313d2b471f9f66fce69d1e2ffed65b0165
Nelson Lopes Silva <5671236+nflsilva@users.noreply.github.com>
2021-08-11 17:01:32 +0100

    Removed dependency on android R class to find resources IDs

--- src/android/com/outsystems/imageeditor/view/ImageEditorActivity.kt
+++ src/android/com/outsystems/imageeditor/view/ImageEditorActivity.kt
@@ -3,17 +3,14 @@ package com.outsystems.imageeditor.view
 import android.net.Uri
 import android.app.Activity
 import android.os.Bundle
-import android.view.WindowManager
-import com.outsystems.rd.LocalCameraSampleApp.R
 
 class ImageEditorActivity : Activity() {
 
-
-    private val editorView by lazy { findViewById<ImageEditorView>(R.id.imageEditorView) }
+    private val editorView by lazy { findViewById<ImageEditorView>(getResourceId("id/imageEditorView")) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_image_editor)
+        setContentView(getResourceId("layout/activity_image_editor"))
 
         intent.extras?.let {
             val inputUri = Uri.parse(it.getString(IMAGE_INPUT_URI_EXTRAS))
@@ -21,16 +18,15 @@ class ImageEditorActivity : Activity() {
             editorView.setInputImageUri(inputUri)
             editorView.setOutputImageUri(outputUri)
         }
-
     }
 
-
+    private fun getResourceId(typeAndName: String): Int {
+        return application.resources.getIdentifier(typeAndName, null, application.packageName)
+    }
 
     companion object {
         const val IMAGE_INPUT_URI_EXTRAS = "IMAGE_EDITOR_IN_URI_EXTRAS"
         const val IMAGE_OUTPUT_URI_EXTRAS = "IMAGE_EDITOR_OUT_URI_EXTRAS"
     }
-
-
 
 }

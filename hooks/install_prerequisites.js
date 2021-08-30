6fd66c4fb9b31b82aa724d1b71c11cdf263b7110
André Grillo <andregrillo@gmail.com>
2021-08-30 11:23:12 +0100

    Removed plugin dependency and extra hook call

--- hooks/install_prerequisites.js
+++ hooks/install_prerequisites.js
@@ -1,5 +1,5 @@
 
-console.log("Running hook to install image editor plugin pre-requisites");
+console.log("Running hook to install plugin node libs requirements");
 
 module.exports = function (context) {
   var child_process = require('child_process'),
